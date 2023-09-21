d9f71b05c8fd3e70921658c494c10e48765d4e6e
Connor Bowling <46806071+ConnorB26@users.noreply.github.com>
2023-09-20 19:52:50 -0500

    Updated page not found

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import styles from './NotFoundPage.module.css';
 
 const NotFoundPage: React.FC = () => {
     return (
-        <Container className="mt-3">
-            <h1>Page Not Found</h1>
+        <Container className={`d-flex justify-content-center align-items-center ${styles.fullHeight}`}>
+            <h1 className='text-white'>Page Not Found</h1>
         </Container>
     );
 };
